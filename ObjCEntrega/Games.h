585ec46471fe3060b9ea2e89735e6051757dd2f1
Larissa Ganaha <larissaganaha@gmail.com>
2018-07-24 12:37:38 -0300

    first commit

---
+++ ObjCEntrega/Games.h
@@ -0,0 +1,13 @@
+//
+//  Games.h
+//  ObjCEntrega
+//
+//  Created by Larissa Ganaha on 24/07/18.
+//  Copyright © 2018 Larissa Ganaha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Games : NSObject
+
+@end
---
+++ ObjCEntrega/Games.m
@@ -0,0 +1,13 @@
+//
+//  Games.m
+//  ObjCEntrega
+//
+//  Created by Larissa Ganaha on 24/07/18.
+//  Copyright © 2018 Larissa Ganaha. All rights reserved.
+//
+
+#import "Games.h"
+
+@implementation Games
+
+@end
